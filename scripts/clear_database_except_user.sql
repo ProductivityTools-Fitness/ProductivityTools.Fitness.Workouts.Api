1133cb6f215b9c0acbc0668bb2436cc2cd413063
Pawel Wujczyk <pwujczyk@gmail.com>
2026-09-17 09:51:28 +0000

    change db name

--- scripts/clear_database_except_user.sql
+++ scripts/clear_database_except_user.sql
@@ -5,7 +5,7 @@
 -- Połączenie z bazą (przy uruchomionym cloud-sql-proxy na porcie 5432):
 --   Host:     localhost
 --   Port:     5432
---   Database: ptfitness
+--   Database: ptfitness-api
 --   User:     fitness (hasło: Jamnik1!) lub postgres (hasło: Pawel123)
 -- ==============================================================================
 
